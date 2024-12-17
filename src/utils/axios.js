7331dd287a82e83361696259298f265bc883ed22
Thiam <khaly_sileye.thiam@edu.univ-fcomte.fr>
2024-12-17 23:44:32 +0100

    Update axios to use environment variable

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // URL de base pour le Back-end
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // URL de base pour le Back-end c
 });
 
 // Intercepteur pour ajouter automatiquement le token JWT
